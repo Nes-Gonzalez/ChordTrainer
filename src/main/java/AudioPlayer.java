953ac6f0b6c2e641c062682054d9cd3ab50485ac
Nes-Gonzalez <N_gonzalez@live.com>
2016-01-11 23:55:02 -0600

    Added playing of sound clip, will need more review

--- src/main/java/AudioPlayer.java
+++ src/main/java/AudioPlayer.java
@@ -1,7 +1,48 @@
+import javax.sound.sampled.*;
+import java.io.File;
 
-public class AudioPlayer {
+public class AudioPlayer implements LineListener{
 
-    public boolean playClip(String chord) {
-        return true;
+    boolean playCompleted = false;
+
+    public boolean playClip(String chord)  {
+
+        File audioFile = new File("./src/test/soundClips/cChord.wav");
+
+        try {
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+            AudioFormat format = audioStream.getFormat();
+            DataLine.Info info = new DataLine.Info(Clip.class, format);
+            Clip audioClip = (Clip) AudioSystem.getLine(info);
+
+            audioClip.addLineListener(this);
+            audioClip.open(audioStream);
+            audioClip.start();
+
+            while (!playCompleted) {
+                // wait for the playback completes
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+            playCompleted = false;
+            audioClip.close();
+            return true;
+
+        } catch (Exception ex) {
+            playCompleted = false;
+            return false;
+        }
+    }
+
+    @Override
+    public void update(LineEvent event) {
+        LineEvent.Type type = event.getType();
+        if (type == LineEvent.Type.STOP) {
+            playCompleted = true;
+        }
     }
 }
